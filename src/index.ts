1e14a69a80fea3c3c86f007d1bd1ec72857f7f48
Vishal Mishra <vishal.mishra@gsynergy.com>
2023-03-13 12:46:49 +0530

    fix enviroment

--- src/index.ts
+++ src/index.ts
@@ -26,22 +26,15 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const REACT_APP_API_KEY = "AIzaSyBejxaOOsOW4ud_-BO2wI0QI59qJquv_Xc";
-const REACT_APP_AUTH_DOMAIN = "dev-quiz-app-090.firebaseapp.com";
-const REACT_APP_PROJECT_ID = "dev-quiz-app-090";
-const REACT_APP_STORAGE_BUCKET = "dev-quiz-app-090.appspot.com";
-const REACT_APP_MESSAGING_SENDER_ID = "262690210009";
-const REACT_APP_APP_ID = "1:262690210009:web:a74fc42f472d2ca8347ec8";
-const REACT_APP_MEASUREMENT_ID = "G-6VWSX4QCEW";
 
 const firebaseConfig = {
-  apiKey: REACT_APP_API_KEY,
-  authDomain: REACT_APP_AUTH_DOMAIN,
-  projectId: REACT_APP_PROJECT_ID,
-  storageBucket: REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-  appId: REACT_APP_APP_ID,
-  measurementId: REACT_APP_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
